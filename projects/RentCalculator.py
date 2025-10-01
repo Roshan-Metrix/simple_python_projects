f0780ec2fef50f3aaa9b5dd26ab5bacb046f3a6c
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-01 21:26:26 +0530

    Rent Calculator project completed

---
+++ projects/RentCalculator.py
@@ -0,0 +1,17 @@
+## Inputs we need from the user 
+# Total rend, food , electricity_spend, charge_per_unit, no of persons
+
+
+
+rent = int(input("Enter your hostel/flat rent :"))
+food = int(input("Enter the amount of food order : "))
+electricity_spend = int(input("Enter the total of electricity spend : "))
+charge_per_unit = int(input("Enter the charge per unit : "))
+persons = int(input("Enter the no. of persons living in room/flat : "))
+
+
+total_bill = electricity_spend * charge_per_unit
+
+output = (food + rent + total_bill) // persons
+
+print("Each person will pay = ", output)
